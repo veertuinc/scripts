73423a32c5d6aa46e41f9d07f648ea8a302eff2e
Nathan Pierce <connarpierce@gmail.com>
2021-01-08 11:27:41 -0500

    better orphan finder logic to support state files

--- registry-orphan-vm-file-finder.bash
+++ registry-orphan-vm-file-finder.bash
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -eo pipefail
-LIST_OF_IN_USE_ANK=($(cat /Library/Application\ Support/Veertu/Anka/registry/vm_dir/*/*/images | uniq))
-ALL_ANK_FILES=($(ls -a /Library/Application\ Support/Veertu/Anka/registry/images_dir | grep -e '^\.*$' -v))
-ANK_FILES_TO_DELETE=($(ls -a /Library/Application\ Support/Veertu/Anka/registry/images_dir | grep -e '^\.*$' -v))
-for in_use_ank in "${LIST_OF_IN_USE_ANK[@]}"; do
-	#echo $in_use_ank
-	for images_dir_ank in "${ALL_ANK_FILES[@]}"; do
-	  if [[ "$images_dir_ank" == "$in_use_ank" ]]; then
-	  	ANK_FILES_TO_DELETE=( "${ANK_FILES_TO_DELETE[@]/$images_dir_ank}" )
+IFS=$'\n'
+LIST_OF_IN_USE_ANK=($(cat /Library/Application\ Support/Veertu/Anka/registry/vm_dir/*/*/images | sort | uniq))
+LIST_OF_IN_USE_ANK+=($(grep -hn "^state_file: " /Library/Application\ Support/Veertu/Anka/registry/vm_dir/*/*/*.yaml | cut -d" " -f2 | sort | uniq))
+ALL_ANK_FILES=($(find /Library/Application\ Support/Veertu/Anka/registry -name "*.ank" | sed -E 's/[[:space:]]/\\\ /g'))
+ANK_FILES_TO_DELETE=($(find /Library/Application\ Support/Veertu/Anka/registry -name "*.ank"| sed 's/\/Library\/Application Support\/Veertu\/Anka\/registry//g'))
+for in_use_ank in ${LIST_OF_IN_USE_ANK[@]}; do
+	for ank in ${ALL_ANK_FILES[@]}; do
+    ank="$(echo $ank | sed 's/\/Library\/Application\\ Support\/Veertu\/Anka\/registry//g')"
+	  if [[ "$ank" =~ $in_use_ank ]]; then
+	  	ANK_FILES_TO_DELETE=( "${ANK_FILES_TO_DELETE[@]/$ank}" )
 	  	break
 	  fi
 	done
 done
 echo "list of orphaned .ank ============================="
-echo "${ANK_FILES_TO_DELETE[@]}"
-
-
+echo "${ANK_FILES_TO_DELETE[@]}" | xargs
+IFS=
