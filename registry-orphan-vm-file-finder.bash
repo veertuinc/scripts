832a12f167d164e45a0bbcaa2c67894d169dc82b
Nathan Pierce <connarpierce@gmail.com>
2021-05-26 16:57:40 -0400

    orphan finder requires / on the end

--- registry-orphan-vm-file-finder.bash
+++ registry-orphan-vm-file-finder.bash
@@ -2,6 +2,7 @@
 set -eo pipefail
 IFS=$'\n'
 [[ -z "$1" ]] && echo "must provide root registry data dir as first arg..." && exit 1
+[[ ! "$1" =~ ^/.*/$ ]] && echo "must start and end with / (be a valid path + ending in /)" && exit 2
 pushd $1 &>/dev/null
 if [[ ! -z "$(ls -A "${1}vm_dir/" 2>/dev/null)" ]]; then
   LIST_OF_IN_USE_ANK=($(cat ./vm_dir/*/*/images | sort | uniq))
