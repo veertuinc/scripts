44853804f4965596e52f203d2bff811003ac02e6
Nathan Pierce <connarpierce@gmail.com>
2021-07-13 20:17:36 -0400

    diagnostics script

---
+++ generate-node-diagnostics.bash
@@ -0,0 +1,78 @@
+#!/bin/bash
+set -eo pipefail
+DIAG_FOLDER_NAME="anka-diagnostics"
+DIAG_PATH="/tmp/${DIAG_FOLDER_NAME}"
+echo "] Collecting Diagnostics from current machine (Please be patient)"
+
+cleanup() {
+  rm -rf "${DIAG_PATH}"
+}
+
+execute() {
+  FILE_NAME="${2:-$1}"
+  FILE_NAME="${FILE_NAME// /_}"
+  FILE_NAME="${FILE_NAME//\//\\}"
+  FILE_NAME="${FILE_NAME//sudo_/}"
+  ( eval "${1}" ) >> "${FILE_NAME}"
+}
+
+execute-multiple-times() {
+  TIMES="${2:-5}"
+  COMMAND=$(echo "${1}" | xargs)
+  COUNT=0
+  while [ "${COUNT}" -le "${TIMES}" ]; do
+    execute "echo \"= $COUNT ========================================\"; ${COMMAND}" "${COMMAND}"
+    sleep 1
+    COUNT=$((COUNT+1))
+  done
+}
+
+copy-files-from-dir() {
+  OLD_IFS=$IFS
+  IFS=$'\n'
+  DIR="${1}"
+  FILTER="${2:-"/*"}"
+  DIR_LOCAL="$(echo ${DIR} | cut -d/ -f2-99)"
+  mkdir -p "${DIAG_PATH}/${DIR_LOCAL}"
+  pushd "${DIAG_PATH}/${DIR_LOCAL}" &>/dev/null
+    for FILE in $(ls -ht ${DIR}/${FILTER} | head -10); do
+      cp -f "${FILE}" .
+    done
+  popd &>/dev/null
+  IFS=$OLD_IFS
+}
+
+echo "]] INFO: This script will perform some commands as root."
+sudo echo ""
+trap cleanup EXIT
+for CUSER in $USER sudo; do
+  mkdir -p "${DIAG_PATH}/${CUSER}"
+  [[ "${CUSER}" == sudo ]] && SUDO="${CUSER}"
+  pushd "${DIAG_PATH}/${CUSER}" &>/dev/null
+    execute-multiple-times "$SUDO df -h" &
+    execute "$SUDO anka config" &
+    execute "$SUDO ls -laht" &
+    execute "$SUDO system_profiler SPHardwareDataType" &
+    execute "$SUDO sysctl -a" &
+    execute-multiple-times "$SUDO iostat" &
+    execute-multiple-times "$SUDO vm_stat" &
+    execute "$SUDO diskutil list" &
+    execute "$SUDO ifconfig" &
+    execute-multiple-times "$SUDO nettop -l 1" &
+    execute-multiple-times "$SUDO ps -axro pcpu | awk \'{sum+=\$1} END {print sum}\'" & # This is a per-core CPU metric, so on a 12 core CPU you can get up to 1200; you're not capped at 100.
+    copy-files-from-dir "$($SUDO anka config log_dir)" &
+    execute "$SUDO ls -laht \"$($SUDO anka config vm_lib_dir)\"" &
+    execute "$SUDO ls -laht \"$($SUDO anka config img_lib_dir)\"" &
+    execute "$SUDO ls -laht \"$($SUDO anka config state_lib_dir)\"" &
+    if [[ "${CUSER}" == sudo ]]; then
+      execute-multiple-times "$SUDO fs_usage -w -t 1" &
+      copy-files-from-dir "/var/log/veertu" "anka_agent.Veertu*" &
+    fi
+    wait
+  popd &>/dev/null
+done
+cd /tmp/
+TAR_NAME="anka-diagnostics.tar.gz"
+tar -czvf $TAR_NAME $DIAG_FOLDER_NAME &>/dev/null
+echo "]] Created $TAR_NAME"
+ls -l $TAR_NAME
